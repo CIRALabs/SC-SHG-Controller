e47c815eef46f16ddf3b9aa5d4afe896b18fc95f
Daniel Innes <Daniel.innes@cira.ca>
2019-06-24 12:16:25 -0400

    proof of concept

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-SC-SHG-Controller
+# SC-SHG-Controller
+CIRA Labs Project information: https://cira.ca/cira-secure-home-gateway
+The process SC-SHG Controller learns of the new association and collects the connection information on the device.
+The device may be new, or may just be reconnecting; both events are interesting.
+Devices are indexed by their layer-2 address (ethernet mac address).
+The process then communicates with the mud-supervisor and updates it’s list of devices and the status of the device. 
+The SC ensures the MUD controller DB is in sync with connected devices (WIFI and LAN based).
---
+++ SC-SHG-Controller.py
@@ -0,0 +1,44 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+import sqlite3
+
+DB_PATH = "/c/Users/daniel.innes/Documents/SHG/production.sqlite3"
+LEASES_FILE = "/c/Users/daniel.innes/Documents/SHG/dhcp.leases"
+LEASES_FILE_PARSE_ORDER = ['unix time', 'mac address', 'ip lease', 'host name', 'unknown']
+
+
+def on_modified(_):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, eui64 FROM devices")
+    sqlite_info = cursor.fetchall()
+    with open(LEASES_FILE, 'r') as leases_file:
+        leases = {lease['host name']: {key: lease[key] for key in ['unix time', 'ip lease', 'mac address']}
+                  for lease in
+                  map(lambda lease: dict(zip(LEASES_FILE_PARSE_ORDER, lease.split(' '))), leases_file.readlines())}
+    dnsmasq_info = [[key, lease['mac address']] for key, lease in leases.items()]
+    devices_to_add = []
+    print(sqlite_info, dnsmasq_info)
+    for info in [x for x in dnsmasq_info if x[1] not in [y[1] for y in sqlite_info]]:
+        unix_time = int(time.time())
+        devices_to_add.append((info[0], info[1], unix_time, unix_time, True))
+    print(devices_to_add)
+    cursor.executemany('INSERT INTO devices (name, eui64, created_at, updated_at, quaranteed) VALUES (?, ?, ?, ?, ?)',
+                       devices_to_add)
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    event_handler = PatternMatchingEventHandler(patterns=['*/' + LEASES_FILE.split('/')[-1]])
+    event_handler.on_modified = on_modified
+    observer = Observer()
+    observer.schedule(event_handler, '/'.join(LEASES_FILE.split('/')[:-1]))
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
