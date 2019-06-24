0ab46a5110cf278e7efe482fcd3349a3763c729e
Daniel Innes <Daniel.innes@cira.ca>
2019-06-24 14:54:52 -0400

    Add devices_to_edit

--- SC-SHG-Controller.py
+++ SC-SHG-Controller.py
@@ -1,7 +1,8 @@
-import time
-from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
 import sqlite3
+import time
+
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.observers import Observer
 
 DB_PATH = "/srv/lxc/mud-supervisor/rootfs/app/fountain/production.sqlite3"
 LEASES_FILE = "/var/dhcp.leases"
@@ -9,23 +10,31 @@ LEASES_FILE_PARSE_ORDER = ['unix time', 'mac address', 'ip lease', 'host name', 
 
 
 def on_modified(_):
+    unix_time = int(time.time())
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute("SELECT name, eui64 FROM devices")
+    cursor.execute("SELECT name, eui64, ipv4 FROM devices")
     sqlite_info = cursor.fetchall()
     with open(LEASES_FILE, 'r') as leases_file:
         leases = {lease['host name']: {key: lease[key] for key in ['unix time', 'ip lease', 'mac address']}
                   for lease in
                   map(lambda lease: dict(zip(LEASES_FILE_PARSE_ORDER, lease.split(' '))), leases_file.readlines())}
-    dnsmasq_info = [[key, lease['mac address']] for key, lease in leases.items()]
+    dnsmasq_info = [[key, lease['mac address'], lease['ip lease']] for key, lease in leases.items()]
     devices_to_add = []
+    devices_to_edit = []
     print(sqlite_info, dnsmasq_info)
-    for info in [x for x in dnsmasq_info if x[1] not in [y[1] for y in sqlite_info]]:
-        unix_time = int(time.time())
-        devices_to_add.append((info[0], info[1], unix_time, unix_time, True))
-    print(devices_to_add)
-    cursor.executemany('INSERT INTO devices (name, eui64, created_at, updated_at, quaranteed) VALUES (?, ?, ?, ?, ?)',
-                       devices_to_add)
+    for info in dnsmasq_info:
+        if info[1] not in [y[1] for y in sqlite_info]:
+            devices_to_add.append((info[0], info[1], info[2], unix_time, unix_time, True))
+        elif not info[2] == sqlite_info[info[1]][2]:
+            devices_to_edit.append((info[2], unix_time, info[0]))
+    print(devices_to_add, devices_to_edit)
+    cursor.executemany(
+        'INSERT INTO devices (name, eui64, ipv4, created_at, updated_at, quaranteed) VALUES (?, ?, ?, ?, ?, ?)',
+        devices_to_add)
+    cursor.executemany(
+        'UPDATE devices SET ipv4 = ?, updated_at = ? WHERE eui64 = ?',
+        devices_to_edit)
     conn.commit()
     conn.close()
 
