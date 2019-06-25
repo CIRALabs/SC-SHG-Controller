bcd178d3ea7c6a95cbc98b691afa724bcb18a69c
Daniel Innes <Daniel.innes@cira.ca>
2019-06-25 10:09:14 -0400

    Add error handling

--- SC-SHG-Controller.py
+++ SC-SHG-Controller.py
@@ -1,42 +1,63 @@
+import os
 import sqlite3
 import sys
 import time
 import logging
 
-logger = logging.getLogger('SC-SHG-Controller')
-logger.setLevel(logging.INFO)
-stream_handler = logging.StreamHandler(sys.stdout)
-stream_handler.setLevel(logging.INFO)
-stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-logger.addHandler(stream_handler)
-dnsmasq_info = sys.argv
-logger.info('new msq from dnsmasq to {0} process: {1}, MAC address: {2}, IP address: {3}, and hostname: {4}'.format(
-    *dnsmasq_info))
-if not dnsmasq_info[1] == 'del':
-    DB_PATH = "/srv/lxc/mud-supervisor/rootfs/app/fountain/production.sqlite3"
-    LEASES_FILE_PARSE_ORDER = ['unix time', 'mac address', 'ip lease', 'host name', 'unknown']
+DNSMASQ_MSG_PARSE_ORDER = ['sc-name', 'process', 'mac', 'ip', 'name']
+DB_PATH = "/srv/lxc/mud-supervisor/rootfs/app/fountain/production.sqlite3"
+# DB_PATH = '/c/Users/daniel.innes/Documents/Repositories/SC-SHG-Controller/testing/testing.sqlite3'
 
-    unix_time = int(time.time())
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute("SELECT name, eui64, ipv4 FROM devices")
-    sqlite_info = cursor.fetchall()
-    logger.debug('info from production db formatted(hostname,MAC address,IP address): {0}'.format(sqlite_info))
-    if dnsmasq_info[2] not in [y[1] for y in sqlite_info]:
-        cursor.execute(
-            'INSERT INTO devices (name, eui64, ipv4, created_at, updated_at, quaranteed) VALUES (?, ?, ?, ?, ?, ?)',
-            (dnsmasq_info[4], dnsmasq_info[2], dnsmasq_info[3], unix_time, unix_time, True))
-        logger.info(
-            'adding new device to production db hostname: {0}, MAC address: {1}, IP address: {2} unix_time: {3}'.format(
-                dnsmasq_info[4], dnsmasq_info[2], dnsmasq_info[3], unix_time))
-    elif not dnsmasq_info[4] == [y[2] for y in sqlite_info if y[1] == dnsmasq_info[2]][0]:
-        cursor.execute('UPDATE devices SET ipv4 = ?, updated_at = ? WHERE eui64 = ?',
-                       (dnsmasq_info[3], unix_time, dnsmasq_info[2]))
-        logger.info(
-            'updated device in production db MAC address: {0}, IP address: {1} unix_time: {2}'.format(dnsmasq_info[2],
-                                                                                                      dnsmasq_info[3],
-                                                                                                      unix_time))
-    else:
-        logger.info('device not updated MAC address: {0}'.format(dnsmasq_info[2]))
-    conn.commit()
-    conn.close()
+
+def process_dnsmasq_info():
+    if not dnsmasq_info['process'] == 'del':
+        unix_time = int(time.time())
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("SELECT name, eui64, ipv4 FROM devices")
+        sqlite_info = cursor.fetchall()
+
+        logger.debug('info from production db formatted(hostname,MAC address,IP address): %s', sqlite_info)
+        if dnsmasq_info['mac'] not in [y[1] for y in sqlite_info]:
+            cursor.execute(
+                'INSERT INTO devices (name, eui64, ipv4, created_at, updated_at, quaranteed) VALUES (?, ?, ?, ?, ?, ?)',
+                (dnsmasq_info['name'], dnsmasq_info['mac'], dnsmasq_info['ip'], unix_time, unix_time, True))
+            logger.info(
+                'adding new device to production db hostname: %s, MAC address: %s, IP address: %s unix_time: %s',
+                dnsmasq_info['name'], dnsmasq_info['mac'], dnsmasq_info['ip'], unix_time)
+        elif not dnsmasq_info['ip'] == [y[2] for y in sqlite_info if y[1] == dnsmasq_info['mac']][0]:
+            cursor.execute('UPDATE devices SET ipv4 = ?, updated_at = ? WHERE eui64 = ?',
+                           (dnsmasq_info['ip'], unix_time, dnsmasq_info['mac']))
+            logger.info(
+                'updated device in production db MAC address: %s, IP address: %s unix_time: %s',
+                dnsmasq_info['mac'], dnsmasq_info['ip'], unix_time)
+        else:
+            logger.info('device not updated MAC address: %s', dnsmasq_info['mac'])
+        conn.commit()
+        conn.close()
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger('SC-SHG-Controller')
+    logger.setLevel(logging.INFO)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(stream_handler)
+
+    dnsmasq_msg = sys.argv
+    logger.info(os.environ)
+
+    try:
+        if len(dnsmasq_msg) == len(DNSMASQ_MSG_PARSE_ORDER):
+            dnsmasq_info = dict(zip(DNSMASQ_MSG_PARSE_ORDER, dnsmasq_msg))
+            logger.info('new msg from dnsmasq to %s process: %s, MAC address: %s, IP address: %s, and hostname: %s',
+                        dnsmasq_info['sc-name'], dnsmasq_info['process'], dnsmasq_info['mac'], dnsmasq_info['ip'],
+                        dnsmasq_info['name'])
+            process_dnsmasq_info()
+        else:
+            logger.error('unable to parse msg from dnsmasq, dnsmasq_msg: %s', dnsmasq_msg)
+    except sqlite3.OperationalError:
+        logger.error('unable to open database file at %s', DB_PATH)
+    except Exception as error:
+        logger.exception('unexpected error processing msg from dnsmasq, dnsmasq_msg: %s', dnsmasq_msg)
