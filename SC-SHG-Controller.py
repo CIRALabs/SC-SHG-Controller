6e10221701069260b69f45d7a4a5438e26ef799c
Daniel Innes <Daniel.innes@cira.ca>
2019-06-27 15:25:32 -0400

    Custom Rake Tasks

--- SC-SHG-Controller.py
+++ SC-SHG-Controller.py
@@ -1,23 +1,39 @@
+import json
 import socket
+import subprocess
 import sys
-import re
+
+
+def check_eui64(eui64: str) -> bool:
+    process = subprocess.Popen('rake getEUI64')
+    return eui64 in process.stdout.readlines()[0]
+
 
 DNSMASQ_MSG_PARSE_ORDER = ['sc-name', 'process', 'mac', 'ip', 'name']
-SERVER_ADDRESS = 'socket4'
-IPV4SEG = r'(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])'
-IPV4ADDR = r'(?:(?:' + IPV4SEG + r'/.){3,3}' + IPV4SEG + r')'
+SERVER_ADDRESS = ''
 dnsmasq_msg = sys.argv
 if len(dnsmasq_msg) == len(DNSMASQ_MSG_PARSE_ORDER):
     dnsmasq_info = dict(zip(DNSMASQ_MSG_PARSE_ORDER, dnsmasq_msg))
     if not dnsmasq_info['process'] == 'del':
-        if re.match(IPV4ADDR, dnsmasq_info['ip']) is None:
+        if ':' in dnsmasq_info['ip']:
             ip_version = 'ipv6'
         else:
             ip_version = 'ipv4'
+        if dnsmasq_info['process'] == 'old':
+            msg = json.dumps({"cmd": "add",
+                              "details": {"mac_addr": dnsmasq_info['mac'], ip_version: dnsmasq_info['ip'],
+                                          "name": dnsmasq_info['name']}}).encode('utf-8')
+        elif check_eui64(dnsmasq_info['mac']):
+            msg = json.dumps({"cmd": "update",
+                              "details": {"mac_addr": dnsmasq_info['mac'], ip_version: dnsmasq_info['ip'],
+                                          "name": dnsmasq_info['name']}}).encode('utf-8')
+        else:
+            msg = json.dumps({"cmd": "add",
+                              "details": {"mac_addr": dnsmasq_info['mac'], ip_version: dnsmasq_info['ip'],
+                                          "name": dnsmasq_info['name']}}).encode('utf-8')
         s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         s.connect(SERVER_ADDRESS)
         try:
-            s.sendall('{ "cmd" : "update", "details" : {"mac_addr" : "{0}", "{1}" : "{2}" ,"name" : "{3}" }}'.format(
-                dnsmasq_info['mac'], ip_version, dnsmasq_info['ip'], dnsmasq_info['name']).encode('utf-8'))
+            s.sendall(msg)
         finally:
             s.close()
